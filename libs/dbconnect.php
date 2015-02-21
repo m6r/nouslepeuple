837f662391d384eb3ef3c6c5093eba6bba2ecc1c
Guillaume Robin <dev@m6r.fr>
2015-02-21 19:40:09 +0100

    cleanup des fichiers

---
+++ libs/dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+define("EZSQL_DB_USER", 'pligg');
+define("EZSQL_DB_PASSWORD", 'Uq5Gt7yxDyRL7Wt9');
+define("EZSQL_DB_NAME", 'pligg');
+define("EZSQL_DB_HOST", 'localhost');
+if (!function_exists('gettext')) {
+	function _($s) {return $s;}
+}
+?>
