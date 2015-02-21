2ea4921f5da2b31e6cead1ed6a9464bce98ea38e
Guillaume Robin <dev@m6r.fr>
2015-02-21 20:16:12 +0100

    default dbconnect file

--- settings.php
+++ settings.php
@@ -1,8 +1,7 @@
 <?php
-$my_base_url = 'http://localhost:8000';
+$my_base_url = 'http://localhost';
 $my_pligg_base = '';
 $dblang = 'en';
-define('table_prefix', 'pligg_');
-$language = 'french';
+if(!defined('table_prefix')){ define('table_prefix', 'pligg_'); }
 include_once mnminclude.'settings_from_db.php';
 ?>
